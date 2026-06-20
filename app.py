231fffd6157c69ffbeb02fa45b46e01ebb662864
Ish Dubey <ish.dubey1203@gmail.com>
2026-06-20 14:18:05 +0530

    Switch Gmail SMTP to SSL

--- app.py
+++ app.py
@@ -23,8 +23,9 @@ app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
 }
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', '')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', '')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME', 'noreply@nexus.app')
